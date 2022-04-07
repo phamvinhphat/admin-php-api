6a7573c9c06e8d294aed8ee081b5ad4deec43114
phamvinhphat <pvphat12c6ntt@gmail.com>
2022-04-07 12:31:39 +0700

    swagger

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use function response;
@@ -259,5 +258,14 @@ class UserRepository implements IUserRepository
             ['message' => 'User successfully signed out'],
             ResponseAlias::HTTP_OK
         );
+    }
+
+    /**
+     * refresh JWT
+     * @return JsonResponse
+     */
+    public function refresh()
+    {
+        return  $this->respondWithToken(auth()->refresh());
     }
 }
